fd73bf34cc7ff89f5b1ae78ec7d1aa2ede9aa19c
Ugurcan Kaya <uurcan@pasilobus.com>
2023-07-22 02:00:42 -0700

    Update src-loader.js
    Signed-off-by: Ugurcan Kaya <uurcan@pasilobus.com>

--- src/utils/src-loader.js
+++ src/utils/src-loader.js
@@ -89,37 +89,8 @@ function parseUrl (src) {
  * @param {function} onResult - Callback with whether `src` is an image.
  */
 function checkIsImage (src, onResult) {
-  var request;
-
-  if (src.tagName) {
-    onResult(src.tagName === 'IMG');
-    return;
-  }
-  request = new XMLHttpRequest();
-
-  // Try to send GET request to check if image first.
-  request.open('GET', src);
-  request.addEventListener('load', function (event) {
-    var contentType;
-    if (request.status >= 200 && request.status < 300) {
-      contentType = request.getResponseHeader('Content-Type');
-      if (contentType == null) {
-        checkIsImageFallback(src, onResult);
-      } else {
-        if (contentType.startsWith('image')) {
-          onResult(true);
-        } else {
-          onResult(false);
-        }
-      }
-    } else {
-      checkIsImageFallback(src, onResult);
-    }
-    request.abort();
-  });
-  request.send();
+  onResult(true);
 }
-
 
 function checkIsImageFallback (src, onResult) {
   var tester = new Image();
